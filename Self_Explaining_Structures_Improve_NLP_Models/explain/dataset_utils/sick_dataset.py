05c04ea0dc896720ece7bf7e34bb91b9969e36ff
Daniil A. Vasilev <vasilev.daniil18@gmail.com>
2021-12-23 18:18:15 +0300

    Fixed SICK

--- Self_Explaining_Structures_Improve_NLP_Models/explain/dataset_utils/sick_dataset.py
+++ Self_Explaining_Structures_Improve_NLP_Models/explain/dataset_utils/sick_dataset.py
@@ -12,6 +12,15 @@ from dataset_utils.collate_functions import collate_to_max_length
 from datasets import load_dataset
 
 class SICKDataset(Dataset):
+    label_map = {
+        "contradiction": 0, 
+        'neutral': 1,
+        "entailment": 2,
+        'A_contradicts_B': 0,
+        'A_neutral_B': 1,
+        'A_entails_B': 2,
+    }
+
     def __init__(self, directory, prefix, bert_path, max_length: int = 512):
         super().__init__()
         self.max_length = max_length
@@ -19,20 +28,6 @@ class SICKDataset(Dataset):
         if prefix == 'dev':
             prefix = 'validation'
         self.dataset = load_dataset('sick')[prefix]
-        '''
-        self.result = []
-
-        label_map = {"contradiction": 0, 'neutral': 1, "entailment": 2}
-        with open(os.path.join(directory, 'snli_1.0_' + prefix + '.jsonl'), 'r', encoding='utf8') as f:
-            lines = f.readlines()
-        self.result = []
-        for line in lines:
-            line_json = json.loads(line)
-            if line_json['gold_label'] not in label_map:
-                # print(line_json['gold_label'])
-                continue
-            self.result.append((line_json['sentence1'], line_json['sentence2'], label_map[line_json['gold_label']]))
-        '''
         self.tokenizer = RobertaTokenizer.from_pretrained(bert_path)
 
     def __len__(self):
@@ -40,7 +35,8 @@ class SICKDataset(Dataset):
 
     def __getitem__(self, idx):
         row = self.dataset[idx]
-        sentence_1, sentence_2, label = row['sentence_A'], row['sentence_B'], row['label']
+        sentence_1, sentence_2 = row['sentence_A'], row['sentence_B']
+        label = self.label_map[row['entailment_AB']]
         # remove .
         if sentence_1.endswith("."):
             sentence_1 = sentence_1[:-1]
